e87f154ced68b49ec8eb3f74c99062b953cb66bd
Mukhtarov-Ramazan <mukhtarovramazan239@gmail.com>
2024-04-30 00:00:09 +0300

    Сделал базовые стили для проекта

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<script setup>
+</script>
+
+<template>
+  <div class="MainPage">
+    <h1>Это мой новый тестовый проект по изучению Nuxt3 и TypeScript</h1>
+  </div>
+</template>
+
+
+<style scoped>
+h1 {
+  font-size: 3.2rem;
+}
+</style>
